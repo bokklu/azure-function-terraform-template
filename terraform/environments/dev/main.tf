babe762f01ffdc04cb5add1b2d2c48b70974a69d
Daniel Buckle <danzanzu@email.com>
2024-02-19 17:19:18 +0100

    Removal of var from module.shared reference

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -4,19 +4,19 @@ module "shared" {
 
 module "resource_group" {
   source = "../../modules/resource-group"
-  application_name = module.shared.var.application_name
+  application_name = module.shared.application_name
 }
 
 module "app_insights" {
   source = "../../modules/app-insights"
-  application_name = module.shared.var.application_name
+  application_name = module.shared.application_name
   azure_resource_group_name = module.resource_group.azure_resource_group_name
   azure_resource_group_location = module.resource_group.azure_resource_group_location
 }
 
 module "function_app" {
   source = "../../modules/function-app"
-  application_name = module.shared.var.application_name
+  application_name = module.shared.application_name
   azure_resource_group_name = module.resource_group.azure_resource_group_name
   azure_resource_group_location = module.resource_group.azure_resource_group_location
   azure_app_insights_instrumentation_key = module.app_insights.azure_app_insights_instrumentation_key
@@ -24,7 +24,7 @@ module "function_app" {
 
 module "key_vault" {
   source = "../../modules/key-vault"
-  application_name = module.shared.var.application_name
+  application_name = module.shared.application_name
   azure_resource_group_name = module.resource_group.azure_resource_group_name
   azure_resource_group_location = module.resource_group.azure_resource_group_location
 }
