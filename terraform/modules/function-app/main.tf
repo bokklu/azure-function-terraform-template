0c49e32c9f34e25b4c28bea0daeab7483e41df52
Daniel Buckle <danzanzu@email.com>
2024-02-27 15:33:04 +0100

    dotnet_version 6.0

--- terraform/modules/function-app/main.tf
+++ terraform/modules/function-app/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_linux_function_app" "poc_function_app" {
 
   site_config {
     application_stack {
-      dotnet_version = "8.0"
+      dotnet_version = "6.0"
     } 
 
     cors {
