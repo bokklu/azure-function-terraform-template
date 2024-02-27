9a0b84106ee1fdaeb25d5ace5b2290c5e5bd9aec
Daniel Buckle <danzanzu@email.com>
2024-02-27 17:19:03 +0100

    azure_resource_group_generated_name

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -1,5 +1,5 @@
 output "resource_group_name" {
-  value = module.resource_group.azure_resource_group_name
+  value = module.resource_group.azure_resource_group_generated_name
 }
 
 output "function_name" {
--- terraform/modules/resource-group/outputs.tf
+++ terraform/modules/resource-group/outputs.tf
@@ -5,3 +5,7 @@ output "azure_resource_group_location" {
 output "azure_resource_group_name" {
     value = local.azure_resource_group_name
 }
+
+output "azure_resource_group_generated_name" {
+    value = azurerm_resource_group.poc_resource_group.name
+}
